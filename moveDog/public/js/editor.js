76ef98c84ae182be0bd85ccbe075ee509eae006b
Hui Kang <sheekang30302@gmail.com>
2022-08-28 02:41:41 +0900

    city, airline, recruit 부분  추가

--- moveDog/public/js/editor.js
+++ moveDog/public/js/editor.js
@@ -1,5 +1,8 @@
 const blogTitleField = document.querySelector('.title');
-const articleFeild = document.querySelector('.article');
+const cityField = document.querySelector('.city');
+const airlineField = document.querySelector('.airline');
+const recruitField = document.querySelector('.recruit');
+const articleField = document.querySelector('.article');
 
 // banner
 const bannerImage = document.querySelector('#banner-upload');
@@ -41,15 +44,15 @@ const uploadImage = (uploadFile, uploadType) => {
 }
 
 const addImage = (imagepath, alt) => {
-    let curPos = articleFeild.selectionStart;
+    let curPos = articleField.selectionStart;
     let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+    articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
 }
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 publishBtn.addEventListener('click', () => {
-    if(articleFeild.value.length && blogTitleField.value.length){
+    if(articleField.value.length && blogTitleField.value.length){
         // generating id
         let letters = 'abcdefghijklmnopqrstuvwxyz';
         let blogTitle = blogTitleField.value.split(" ").join("-");
@@ -65,7 +68,10 @@ publishBtn.addEventListener('click', () => {
         //access firstore with db variable;
         db.collection("blogs").doc(docName).set({
             title: blogTitleField.value,
-            article: articleFeild.value,
+            city: cityField.value,
+            airline: airlineField.value,
+            recruit: recruitField.value,
+            article: articleField.value,
             bannerImage: bannerPath,
             publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
         })
