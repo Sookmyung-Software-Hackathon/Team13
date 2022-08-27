73f01549c200580b15ac726afb841295365885a5
Hui Kang <sheekang30302@gmail.com>
2022-08-27 18:42:50 +0900

    Add files via upload

---
+++ moveDog/public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBpuY1GmLimqDJElpXglYwbBz___vFAbnI",
+    authDomain: "dogvolunteer-c2f9d.firebaseapp.com",
+    projectId: "dogvolunteer-c2f9d",
+    storageBucket: "dogvolunteer-c2f9d.appspot.com",
+    messagingSenderId: "1076447481653",
+    appId: "1:1076447481653:web:4ee39f7aaf53c6fde3867c"
+}; 
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
