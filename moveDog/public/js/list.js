5efae4888c2e5a268bb8e06a766a7100b98a5dfb
alotofhee <55499429+alotofhee@users.noreply.github.com>
2022-08-28 16:18:02 +0900

    Update list.js

--- moveDog/public/js/list.js
+++ moveDog/public/js/list.js
@@ -17,7 +17,6 @@ const createBlog = (blog) => {
         <br>
         <a class="btn dark">${data.city}</a>
         <a class="btn dark">${data.airline}</a>
-        <a class="btn dark">${data.address}</a>
         <a class="btn dark">${data.recruit}</a>
 
         <a href="/${blog.id}" class="btn dark">read</a>
